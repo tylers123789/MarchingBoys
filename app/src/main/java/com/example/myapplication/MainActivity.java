ea7d5adb1e89d0b028f40ccca522e4d46c6f1d92
tylers123789 <53475525+tylers123789@users.noreply.github.com>
2020-09-26 18:20:48 -0700

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+//s
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
